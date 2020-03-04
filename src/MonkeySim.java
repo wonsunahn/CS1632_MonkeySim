6e1333c15f55a117bdf7fc4701d7290000d04688
wonsunahn <wahn@pitt.edu>
2020-03-03 21:33:14 -0500

    Update MonkeySim.java

--- src/MonkeySim.java
+++ src/MonkeySim.java
@@ -115,7 +115,7 @@ public class MonkeySim {
 			Monkey m = ml.get(j);
 			if (m.hasBanana()) {
 				int k = 0;
-				int bar = 100;
+				int bar = 10000;
 				while (k++ < (bar * bar)) {
 					if (m.getMonkeyNum() == k) {
 						bar -= Math.round(Math.sqrt(bar));
